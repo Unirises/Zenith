f3cb89fddf66664c7b7e0299da61a5d5a5742dce
Topher-06 <christopherfrancisco171@gmail.com>
2022-02-02 15:28:38 +0800

    new

--- sw.js
+++ sw.js
@@ -1,46 +1,53 @@
-const cacheName = 'zenith';
-const staticAssets = [
-    './',
-    './index.php',
-    './assets/css/styles.css',
-    './assets/js/zenith.js'
-];
-
-self.addEventListener('install', async e => {
-    const cache = await caches.open(cacheName);
-    await cache.addAll(staticAssets);
-    return self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-    self.clients.claim();
-});
-
-self.addEventListener('fetch', async e => {
-    const req = e.request;
-    const url = new URL(req.url);
-
-    if (url.origin === location.origin) {
-        e.respondWith(cacheFirst(req));
-    } else {
-        e.respondWith(networkAndCache(req));
-    }
-});
-
-async function cacheFirst(req) {
-    const cache = await caches.open(cacheName);
-    const cached = await cache.match(req);
-    return cached || fetch(req);
-}
-
-async function networkAndCache(req) {
-    const cache = await caches.open(cacheName);
-    try {
-        const fresh = await fetch(req);
-        await cache.put(req, fresh.clone());
-        return fresh;
-    } catch (e) {
-        const cached = await cache.match(req);
-        return cached;
-    }
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+if (workbox) {
+    console.log("Yay! Workbox is loaded !");
+    workbox.precaching.precacheAndRoute([]);
+    /*  cache images in the e.g others folder; edit to other folders you got
+       and config in the sw-config.js file
+        */
+    workbox.routing.registerRoute(
+        /(.*)others(.*)\.(?:png|gif|jpg)/,
+        new workbox.strategies.CacheFirst({
+            cacheName: "images",
+            plugins: [
+                new workbox.expiration.Plugin({
+                    maxEntries: 50,
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+                })
+            ]
+        })
+    );
+    /* Make your JS and CSS âš¡ fast by returning the assets from the cache,
+  while making sure they are updated in the background for the next use.
+  */
+    workbox.routing.registerRoute(
+        // cache js, css, scc files
+        /.*\.(?:css|js|scss|)/,
+        // use cache but update in the background ASAP
+        new workbox.strategies.StaleWhileRevalidate({
+            // use a custom cache name
+            cacheName: "assets",
+        })
+    );
+    // cache google fonts
+    workbox.routing.registerRoute(
+        new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
+        new workbox.strategies.CacheFirst({
+            cacheName: "google-fonts",
+            plugins: [
+                new workbox.cacheableResponse.Plugin({
+                    statuses: [0, 200],
+                }),
+            ],
+        })
+    );
+    // add offline analytics
+    workbox.googleAnalytics.initialize();
+    /* Install a new service worker and have it update
+    and control a web page as soon as possible
+    */
+    workbox.core.skipWaiting();
+    workbox.core.clientsClaim();
+} else {
+    console.log("Oops! Workbox didn't load ðŸ‘º");
 }
