4d4fddaef251bd936bc6cb8e6d997dbb3142cdb1
Topher-06 <christopherfrancisco171@gmail.com>
2022-02-02 16:08:13 +0800

    new

--- sw.js
+++ sw.js
@@ -1,3 +1,12 @@
-self.addEventListener("fetch", event => {
-    console.log("You fetched " + event.url);
+let cache_name = "SimiCart"; // The string used to identify our cache
+self.addEventListener("install", event => {
+    console.log("installing...");
+    event.waitUntil(
+        caches
+        .open(cache_name)
+        .then(cache => {
+            return cache.addAll(assets);
+        })
+        .catch(err => console.log(err))
+    );
 });
