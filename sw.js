d6f7cabc7b1de88adad76217f803f82d0c4539df
Topher-06 <christopherfrancisco171@gmail.com>
2022-02-02 17:29:35 +0800

    new push

--- sw.js
+++ sw.js
@@ -1,7 +1,18 @@
-const CACHE_NAME = 'webapp-v1';
+const CACHE_NAME = 'Zenith-V1';
 const CACHE_FILES = [
     '/zenith/index.php',
-    '/zenith/assets/js/zenith.js'
+    '/zenith/assets/css/style.css',
+    '/zenith/assets/js/zenith.css',
+    '/zenith/pages/navigation.php',
+    '/zenith/pages/p1.php',
+    '/zenith/pages/p2.php',
+    '/zenith/pages/p3.php',
+    '/zenith/pages/p4.php',
+    '/zenith/pages/p5.php',
+    '/zenith/pages/p6.php',
+    '/zenith/assets/img/bg/bg1.png',
+    '/zenith/assets/img/bg/bg2.png',
+    '/zenith/assets/img/bg/bg3.png',
 ];
 
 self.addEventListener('install', event => {
