a6bd7bdd0844f53aa1965a64bbc5882edb500d62
Topher-06 <christopherfrancisco171@gmail.com>
2022-02-02 17:39:06 +0800

    new

--- sw.js
+++ sw.js
@@ -10,7 +10,9 @@ const CACHE_FILES = [
     '/zenith/pages/p4.php',
     '/zenith/pages/p5.php',
     '/zenith/pages/p6.php',
-    '/zenith/assets/img/bg/bg1.png'
+    '/zenith/assets/img/bg/bg1.png',
+    '/zenith/assets/img/bg/bg2.png',
+    '/zenith/assets/img/bg/bg3.png'
 ];
 
 self.addEventListener('install', event => {
