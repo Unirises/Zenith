7c7c509ce9930fc10f7a45f126c0d463acaf8b23
Topher-06 <christopherfrancisco171@gmail.com>
2022-02-02 16:40:03 +0800

    new push

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,12 @@
 window.addEventListener('load', () => {
-    if ("serviceWorker" in navigator) {
-        navigator.serviceWorker
-            .register("sw.js")
-            .then(() => console.log("registered service worker!"));
+    if ('serviceWorker' in navigator) {
+        console.log('CLIENT: service worker registration in progress.');
+        navigator.serviceWorker.register('/service-worker.js').then(function() {
+            console.log('CLIENT: service worker registration complete.');
+        }, function() {
+            console.log('CLIENT: service worker registration failure.');
+        });
+    } else {
+        console.log('CLIENT: service worker is not supported.');
     }
 });
