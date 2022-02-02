7fc192d2cd4efafb61c07a8baffc8b13316f0115
Topher-06 <christopherfrancisco171@gmail.com>
2022-02-02 16:05:52 +0800

    new

--- sw.js
+++ sw.js
@@ -1,22 +1,3 @@
-let cache_name = 'zenith';
-const staticAssets = [
-    './',
-    './index.php',
-    './assets/css/styles.css',
-    './assets/js/zenith.js'
-];
-self.addEventListener("install", event => {
-    console.log("installing...");
-    event.waitUntil(
-        caches
-        .open(cache_name)
-        .then(cache => {
-            return cache.addAll(staticAssets);
-        })
-        .catch(err => console.log(err))
-    );
-});
-
 self.addEventListener("fetch", event => {
     console.log("You fetched " + event.url);
 });
