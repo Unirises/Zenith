6082bc8b6a6305eb3858997b00e250d5e47c1885
Topher-06 <christopherfrancisco171@gmail.com>
2022-04-01 11:36:19 -0700

    new

--- pages/p3.php
+++ pages/p3.php
@@ -6,6 +6,6 @@
     <div class="d-flex flex-column text-right">
         <div id="p3_trans1"><img src="assets/img/resources/dosage/for_pneumonia.png" id="for_pneumoniaBtn" class=" cursor_pointer animate__animated animate__fadeInTopRight" style="height:32vh; width:45%;" alt=""></div>
         <div id="p3_trans2"><img src="assets/img/resources/dosage/better_than_10_days.png" id="better_than_10_daysBtn" style="height:32vh; width:60%;" class=" cursor_pointer animate__animated animate__fadeInRight" alt=""></div>
-        <div id="p3_trans3"><img src="assets/img/resources/dosage/co-amoxiclav.png" id="co_amoxiclavBtn" class=" cursor_pointer animate__animated animate__fadeInBottomRight" style="height:32vh; width:78%;" alt=""></div>
+        <div id="p3_trans3"><img src="assets/img/resources/dosage/co-amoxiclav.png" id="co_amoxiclavBtn" class=" cursor_pointer animate__animated animate__fadeInBottomRight" style="height:30vh; width:78%;" alt=""></div>
     </div>
 </div>
